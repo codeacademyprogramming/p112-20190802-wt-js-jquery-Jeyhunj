5cfd7c239dc2f455bb8ef05878e7d2580052497d
Jeyhunj <jeyhunmj@code.edu.az>
2019-08-05 07:33:01 +0400

    html and jquery modified

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,18 +1,22 @@
 $(function () {
 
-    
-
-    $("body").prepend("<nav>")
 
 
-    $("#script").after(`<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  // Bootstrap scripts and link
+
+  $("#script").after(`<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>`)
+  $("title").after(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">`)
+  
+  
+  // Nav
 
-    $("nav").addClass("navbar navbar-expand-lg navbar-dark bg-dark fixed-top")
+  $("body").prepend("<nav>")
+  $("nav").addClass("navbar navbar-expand-lg navbar-dark bg-dark fixed-top")
 
-    $("nav").prepend(`<div class="container">
-    <a class="navbar-brand" href="#">Start Bootstrap</a>
+  $("nav").prepend(`<div class="container">
+    <a class="navbar-brand" href="#">Bootstrap Template</a>
     <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -36,9 +40,16 @@ $(function () {
     </div>
   </div>`);
 
-    $("nav").after("<div class = t>")
-    $(".t").addClass("container")
-    $(".t").prepend(`
+
+
+  //  PAGE CONTENT
+
+  // Header
+ 
+ 
+  $("nav").after("<div class = conheader>")
+  $(".conheader").addClass("container")
+  $(".conheader").prepend(`
 
   <header class="jumbotron my-4">
     <h1 class="display-3">A Warm Welcome!</h1>
@@ -48,10 +59,12 @@ $(function () {
   
 `)
 
-$("header").after(`<div class="row text-center">
+  $("header").after("<main>")
+
+  $("main").prepend(`<div class="row text-center">
 <div class="col-lg-3 col-md-6 mb-4">
   <div class="card h-100">
-    <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2019/07/08/04/23/traveling-4323759_960_720.png" alt="">
     <div class="card-body">
       <h4 class="card-title">Card title</h4>
       <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
@@ -64,7 +77,7 @@ $("header").after(`<div class="row text-center">
 
 <div class="col-lg-3 col-md-6 mb-4">
   <div class="card h-100">
-    <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2019/07/08/04/23/traveling-4323759_960_720.png" alt="">
     <div class="card-body">
       <h4 class="card-title">Card title</h4>
       <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
@@ -77,7 +90,7 @@ $("header").after(`<div class="row text-center">
 
 <div class="col-lg-3 col-md-6 mb-4">
   <div class="card h-100">
-    <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2019/07/08/04/23/traveling-4323759_960_720.png" alt="">
     <div class="card-body">
       <h4 class="card-title">Card title</h4>
       <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
@@ -90,7 +103,7 @@ $("header").after(`<div class="row text-center">
 
 <div class="col-lg-3 col-md-6 mb-4">
   <div class="card h-100">
-    <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2019/07/08/04/23/traveling-4323759_960_720.png" alt="">
     <div class="card-body">
       <h4 class="card-title">Card title</h4>
       <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
@@ -103,14 +116,17 @@ $("header").after(`<div class="row text-center">
 
 </div>`)
 
-$(".t").after("<footer>")
-$("footer").addClass("py-5 bg-dark")
-$("footer").prepend(`<div class="container">
+  // Footer
+
+  $(".conheader").after("<footer>")
+  $("footer").addClass("py-5 bg-dark")
+  $("footer").prepend(`<div class="container">
 <p class="m-0 text-center text-white">Copyright © Your Website 2019</p>
 </div>`)
 
+  // Css between Nav and Header and Bg
 
-$("body").css("padding-top", "56px")
-
+  $("body").css("padding-top", "56px");
+  $("body").css("background-image", "linear-gradient(#a6ceff, #ff0101)");
 });
 
